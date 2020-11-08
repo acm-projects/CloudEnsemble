ad2c79cd4bced3b90507dfb2b4a6598957f40908
franciscaxli <70875969+franciscaxli@users.noreply.github.com>
2020-11-08 16:58:20 -0600

    Add files via upload
    Updated files for landing page

--- App.js
+++ App.js
@@ -15,46 +15,49 @@ function App() {
       </header>
 
       <body className="body">
-      
+      <div>
       <img src={microphone} className="Main-picture" alt="microphone"/>
+      </div>
       <button className = "btn-outline">LOG IN</button>
 
+      <div>
       <h1 className= "text-title">
           Welcome to <br></br>Cloud Ensemble!
       </h1>
       <h4 className= "text-sub">
           Upload clips, collaborate, and mix + match <br></br> to make your next track!
       </h4>
+      <button className ="btn-main"> SIGN UP </button>
       <h4 className="text-sub-center">
         Making music together is as easy as 1-2-3!
       </h4>
-      <button className ="btn-main"> SIGN UP </button>
+      </div>
       </body>
 
-      <thirds>
-        <p className="text-third-left">
-          <h1>1</h1><br></br>
+      <thirds className = "text-thirds">
+        <p>
+          <h1>1</h1>
           Create an account to access <br></br>
           your dashboard and upload <br></br>
           your clips.
         </p>
 
-        <p className="text-third-middle">
-          <h1>2</h1> <br></br>
+        <p>
+          <h1>2</h1>
           Browse and use different <br></br>
           clips to create your newest <br></br>
           track.
         </p>
 
-        <p className="text-third-right">
-          <h1>3</h1> <br></br>
+        <p>
+          <h1>3</h1>
           Collaborate, share with<br></br>
           friends, and review other <br></br>
           people's tracks.
         </p>
       </thirds>
 
-      <callToAction>
+      <div>
         <h4 className = "text-sub-center-2">
           Ready to get started?
         </h4>
@@ -63,8 +66,8 @@ function App() {
         <h5 className = "text-sub-center-3">
           Already have an account? Log in! 
         </h5>
+        </div>
 
-      </callToAction>
     </div>
     
   );
